b82de81ec6bb8d1cdecc1cb5b413ec8c2cf798ab
KeMura-ops <kenti432@yahoo.co.jp>
2021-05-19 23:25:26 +0900

    同期通信でのいいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root 'home#index'
+
+  # いいねのルーティング
+  post 'like/:id' => 'likes#create', as: 'create_like' # いいねをするとき
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like' # いいねを外すとき
 end
